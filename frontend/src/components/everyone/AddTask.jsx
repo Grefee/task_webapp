b604cc22c3b5a93f863cd5b88da1b44321aed377
Pavel Hujer <101639003+Grefee@users.noreply.github.com>
2023-03-23 08:18:46 +0100

    Update AddTask.jsx

--- frontend/src/components/everyone/AddTask.jsx
+++ frontend/src/components/everyone/AddTask.jsx
@@ -68,8 +68,7 @@ function InsertNewTask({refetchTasks}){
         }),
     });
 
-    const addNewTask = () => {
-        
+    const addNewTask = () => { 
         let htmlRequest = document.getElementById("requester").value;
         let htmlTaskTo = document.getElementById("taskto").value;
         let htmlDescription = document.getElementById("description").value;
@@ -77,15 +76,12 @@ function InsertNewTask({refetchTasks}){
         let htmlDate = document.getElementById("date").value;
         let htmlFileLink = document.getElementById("filelink").value;
         let htmlEmail = document.getElementById("email").value;
-
-
+        
         if (htmlRequest.trim() !== '' && htmlTaskTo.trim() !== '' &&  htmlTime.trim() !== '' &&  htmlDate.trim() !== '' &&  htmlDescription.trim() !== '' && htmlEmail.trim() !== '') {
             let htmlFinalDateTime = new Date(htmlDate + ' ' + htmlTime);
             let utcPlusOneDate = new Date(htmlFinalDateTime.valueOf() + 60 * 60 * 1000);
             let formattedUtcPlusOneDate = utcPlusOneDate.toISOString().replace('T', ' ').replace(/\.\d{3}Z/, '');
-
             mutate({htmlRequest, htmlTaskTo, htmlDescription, formattedUtcPlusOneDate, htmlFileLink, htmlEmail});
-
           } else {
             Swal.fire({
                 icon: "error",
@@ -94,11 +90,12 @@ function InsertNewTask({refetchTasks}){
                 }) 
         };
     };
+    
     return (
         <button className="button-28" onClick={addNewTask}>create task</button>
       );
-    
 }
+
 async function fetchActiveHistory(IP) {
     const response = await fetch(`http://${IP}:${ipPort}/getActiveHistory`, {
         method: 'GET',
@@ -106,28 +103,24 @@ async function fetchActiveHistory(IP) {
         return response.json();
   }
 
-
 function Back() {
-const navigate = useNavigate();
-const back = () => {
-    sessionStorage.setItem('acc_type', '');
-    sessionStorage.setItem('user_name', '');
-    navigate('/');
-    
+    const navigate = useNavigate();
+    const back = () => {
+        sessionStorage.setItem('acc_type', '');
+        sessionStorage.setItem('user_name', '');
+        navigate('/');
 }
 return (
     <button className="button-28 max-w-fit" onClick={() => back()}>Back</button>
-)
+    )
 }
 
 
-function AddNew() {
-    
+function AddNew() { 
     const {isLoading: taskHistoryLoadingStatus, data: tasksHistory, refetch: refetchTasksHistory} = useQuery({queryKey: ['tasksHistory'], queryFn: async () => await fetchActiveHistory(IP), initialData: []});
     const {isLoading: taskLoadingStatus, data: tasks, refetch: refetchTasks} = useQuery({queryKey: ['tasks'], queryFn: async () => await fetchActiveTasks(IP), initialData: []});
     const socket = useSocket();
     const [expandedTasks, setExpandedTasks] = useState([]);
-    
     useEffect(() => {
         if (socket) {
             // refetch from outside update
@@ -146,13 +139,10 @@ function showHistory(task_id) {
       setExpandedTasks([...expandedTasks, task_id]);
     }
   };
-
-
 if (taskLoadingStatus || taskHistoryLoadingStatus) {
     return <span>Loading...</span>
     }
 return (
-
     <div className="flex flex-col justify-start h-fit items-center">
         <div className="flex flex-row justify-evenly h-auto p-8 my-0 w-full mt-0 bg-gradient-to-r from-gray-700 to-gray-500 ">
             <div></div>
@@ -173,12 +163,10 @@ return (
                     <input id="requester" type="text" className="bg-white border-2 p-2  shadow-inner border-gray-200 rounded-md"></input>
                 </div>
             </div>
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl">
                 <div className="flex  w-1/4 align-middle py-2 justify-center">
                     <label>Task To:</label>
                 </div>
-
                 <div className="flex  w-3/4 align-middle justify-start">
                   <select className="bg-white border-2 p-2  shadow-inner border-gray-200 rounded-md" name="taskto"  id="taskto" required>
                     <option key="" value="" disabled defaultValue hidden></option>
@@ -188,8 +176,6 @@ return (
                     </select> 
                 </div>
             </div>
-
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl">
                 <div className="flex  w-1/4 align-middle py-2 justify-center">
                     <label>Description:</label>
@@ -198,7 +184,6 @@ return (
                     <textarea id="description" className="p-2 h-24 w-11/12 resize-none overflow-auto break-words bg-white border-2  shadow-inner border-gray-200 rounded-md"></textarea>
                 </div>
             </div>
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl">
                 <div className="flex  w-1/4 align-middle py-2 justify-center">
                     <label>Final Date and Time:</label>
@@ -208,7 +193,6 @@ return (
                     <input id="time" type="time" className="bg-white border-2 p-2 ml-5 shadow-inner border-gray-200 rounded-md" />
                 </div>
             </div>
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl">
                 <div className="flex  w-1/4 align-middle py-2 justify-center">
                     <label>File Link:</label>
@@ -217,7 +201,6 @@ return (
                     <textarea id="filelink" className="p-2 h-24 w-11/12 resize-none overflow-auto break-words bg-white border-2  shadow-inner border-gray-200 rounded-md"></textarea>
                 </div>
             </div>
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl">
                 <div className="flex  w-1/4 align-middle py-2 justify-center">
                     <label>Email:</label>
@@ -226,14 +209,11 @@ return (
                     <textarea id="email" className="p-2 h-24 w-11/12 resize-none overflow-auto break-words bg-white border-2  shadow-inner border-gray-200 rounded-md"></textarea>
                 </div>
             </div>
-
             <div className="flex flex-row w-4/6 mx-9 my-3  rounded-2xl justify-end">
                 <div className="flex w-1/4 align-middle py-2 pr-10 justify-center">
                     <InsertNewTask refetchTasks={refetchTasks}/>
                 </div>
-                
             </div>
-
        </div>
        {/* active tasks table */}
        <div className="bg-slate-200 w-11/12 mt-7 rounded-3xl border border-slate-400 shadow-lg divide-black overflow-hidden mb-32 ">
@@ -279,8 +259,7 @@ return (
                                       <tr>
                                         <th colSpan="2" className="py-4">History tasks for: {item.task_id}</th>
                                       </tr>
-                                      <tr>
-                                        
+                                      <tr>                                        
                                         <th className="p-2">Change made</th>
                                         <th className="p-2">By</th>
                                         <th className="p-2">Requester</th>
@@ -290,7 +269,6 @@ return (
                                         <th className="p-2">Comment</th>
                                         <th className="p-2">File Link</th>
                                         <th className="p-2">Email</th>
-                                      
                                       </tr>
                                     </thead>
                                     <tbody>
@@ -324,15 +302,11 @@ return (
                           </React.Fragment>
                         )}
                         )} 
-
-                        
                     </tbody>
                 </table>
             </div>  
     </div>
     );
 }
-
-
 
 export default AddNew;
