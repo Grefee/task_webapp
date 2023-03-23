8bbf9efa2a21ddf057036d265b3f00a16befbf93
Pavel Hujer <101639003+Grefee@users.noreply.github.com>
2023-03-23 08:19:54 +0100

    Update Home.jsx

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -39,9 +39,8 @@ function LogIn() {
             "username": htmlUser,
             "password": htmlPwd,
         }),
-      }).then(response => response.json()); // add this line to extract the JSON data
+      }).then(response => response.json());
     },
-     
     onSuccess: (data) =>{
       if (data.message === 'User not found' || data.message === 'Wrong password' || data.message === 'User not active'){
         Swal.fire({
@@ -73,13 +72,9 @@ function LogIn() {
     }
   });
 
-
-
   const logInClick = () => {
-
     let htmlUser = document.getElementById("login").value;
     let htmlPwd = document.getElementById("password").value;
-
     if (htmlUser.trim() !== '' && htmlPwd.trim() !== ''){
       mutate({htmlUser, htmlPwd})
     } else {
@@ -91,7 +86,6 @@ function LogIn() {
     };
   }
 
-  
   return (            
     <button className="bg-red-500 mb-2 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
       onClick={logInClick}
@@ -101,26 +95,18 @@ function LogIn() {
   )
 }
 
-
 function Home() {
   const navigate = useNavigate();
-
   const [error, setError] = useState('');
-
-
   const toNewTaskClick = () => {
     //socket.emit('test', 'hello from the front-end');
     navigate(`/AddNew`)
   }
 
-
-
-  return (
-    
+  return (  
     <div className="flex flex-col justify-start w-90 h-screen items-center">
       <div className='text-center p-8 my-0 w-full mt-0 bg-gradient-to-r from-gray-700 to-gray-500 '>
         <h1 className="text-3xl text-white">SCM requests - SCM požadavky</h1>
-      
       </div>
         <div className="flex h-auto my-80 w-96 items-center justify-center bg-gradient-to-br from-gray-600 to-blue-500 rounded-3xl shadow-2xl">
           <div className="flex flex-col  mx-9 my-3 rounded-2xl">
@@ -132,7 +118,6 @@ function Home() {
                     <input type="text" id="login" name="login" required  className="bg-white border-2 p-2  shadow-inner border-gray-200 rounded-md" /> 
                   </div>
             </div>
-
             <div className="flex flex-row mb-2 mt-5 ">
                   <div className="flex align-middle py-2 pr-2 justify-center text-white">
                       <label>Password: </label> 
@@ -141,28 +126,19 @@ function Home() {
                       <input type="password" id="password" name="password" required className="bg-white border-2 p-2  shadow-inner border-gray-200 rounded-md" />
                   </div>
             </div>
-
             <LogIn />
-            
             <span className="bg-black h-1 my-2"></span>
-
             <button className="bg-red-500 my-2 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-8 rounded-full"
             onClick={(event) => toNewTaskClick(event)}
             >
               Create new Task
             </button>
           </div>
-
           <div>
-
           </div>
         </div>
     </div>
   );
 }
 
-
 export default Home;
-
-
-
